e5b40b10b5db069206b07703435323008de78d65
MacStudent <macstudent@128-IMAC28.local>
2020-01-21 20:48:45 -0500

    [KRI] Initial commit

---
+++ src/main/java/com/lambton/FirstExample.java
@@ -0,0 +1,11 @@
+package com.lambton;
+
+public class FirstExample
+{
+    public static void main(String[] args) {
+        System.out.println("Welcome to JAVA Programming");
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Kritima");
+        }
+    }
+}
